81958297559e5c96ec1305ae0527df8fe4e7ce66
colorpen2010 <melnik-olgaaa@ukr.net>
2021-09-27 18:02:31 +0300

    2 player game version 0.1

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import wrap
 wrap.world.create_world(1000,1000)
 wrap.world.set_title('2 player game')
 wrap.world.set_back_color(25,150,237)
+wrap.sprite.add('mario-1-big',500,500,'stand')
+wrap.sprite.add('mario-2-big',550,500,'stand')
