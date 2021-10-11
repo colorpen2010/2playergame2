3df86321799a2d2a294bc6debf4d5ff18166e56d
colorpen2010 <melnik-olgaaa@ukr.net>
2021-10-11 18:00:41 +0300

    sidet version 0.2.2

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ wrap.world.set_title('2 player game')
 wrap.world.set_back_color(25, 150, 237)
 # ground
 ground = wrap.sprite.add('mario-scenery', 500, 10, 'ground')
-wrap.sprite.set_size(ground, 1000, 470)
+wrap.sprite.set_size(ground, 700, 470)
 wrap.sprite.move_bottom_to(ground, 1000)
+
 # player1
 player1 = wrap.sprite.add('mario-1-big', 500, 500, 'stand')
 # player2
 player2 = wrap.sprite.add('mario-2-big', 550, 500, 'stand')
 wrap.sprite.set_reverse_x(player2, True)
-speedy = -4
+speedy2 = -0
+wrap.sprite.set_reverse_x(player1, True)
+speedy1 = -0
 
 
 def walk(p):
@@ -25,17 +28,41 @@ def walk(p):
         wrap.sprite.set_costume(p, 'walk3')
     elif cos2 == 'walk3':
         wrap.sprite.set_costume(p, 'walk1')
-@wrap.on_key_down(wrap.K_UP)
-def jump():
 
-    global speedy
-    speedy=-4
+@wrap.on_key_down(wrap.K_DOWN)
+def sit2():
+    wrap.sprite.set_costume(player2, 'duck')
+
+@wrap.on_key_down(wrap.K_UP)
+def jump2():
+    global speedy2
+    player2y = wrap.sprite.get_y(player2)
+    if player2y == 500:
+        speedy2 = -4
+
+
+@wrap.on_key_down(wrap.K_w)
+def jump1():
+    global speedy1
+    player1y = wrap.sprite.get_y(player1)
+    if player1y == 500:
+        speedy1 = -4
+
 
 @wrap.always(25)
 def down():
-    global speedy
-    speedy+=0.1
-    wrap.sprite.move(player2, 0, speedy)
+    global speedy2, speedy1
+    print(speedy1, speedy2)
+    speedy2 += 0.1
+    wrap.sprite.move(player2, 0, speedy2)
+    if wrap.sprite.is_collide_sprite(player2, ground):
+        speedy2 = 0
+        wrap.sprite.move_to(player2, wrap.sprite.get_x(player2), 500)
+    speedy1 += 0.1
+    wrap.sprite.move(player1, 0, speedy1)
+    if wrap.sprite.is_collide_sprite(player1, ground):
+        speedy1 = 0
+        wrap.sprite.move_to(player1, wrap.sprite.get_x(player1), 500)
 
 
 # hodba2
